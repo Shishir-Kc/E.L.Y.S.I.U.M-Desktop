9257ab4263a2723e2413e0ddf7ed761a218f8d2b
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-07-05 21:37:49 +0545

    Add React Native Android project (Kotlin, Gradle, manifests)

---
+++ android/app/src/main/java/com/elysium/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.elysium
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  override fun getMainComponentName(): String = "elysium"
+
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
